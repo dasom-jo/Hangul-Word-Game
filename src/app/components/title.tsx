1f4d845098d3b6d8e85d33fafaed28da415cbfd4
jodasom <jodasom0216@gmail.com>
2025-02-17 09:32:45 +0900

    feat : 데탑 버전 css 구현 완료

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -3,11 +3,16 @@ import styles from "./title.module.css";
 const Title = () => {
   return (
     <div>
+      {/* 상소문 스타일의 제목 표지 */}
+      {/* 타이틀의 상단 봉 */}
       <div className={styles.bong1}></div>
+      {/* 상소문의 글자 페이지 */}
       <div className={styles.page}>
+        {/* 제목 */}
         <div className={styles.text_1}>한국어</div>
         <div className={styles.text_2}>낱말 게임</div>
       </div>
+      {/* 상소문의 하단 봉 */}
       <div className={styles.bong2}></div>
     </div>
   );
