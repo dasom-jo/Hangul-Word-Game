258b0dbb6d65471797a47e7a3ea440a1af7dd994
jodasom <jodasom0216@gmail.com>
2025-02-17 12:05:06 +0900

    feat : 카카오 css 이미지 구현 완료

--- src/app/components/KakaoLoginButton.tsx
+++ src/app/components/KakaoLoginButton.tsx
@@ -4,19 +4,21 @@ import styles from "./KakaoLoginButton.module.css";
 
 const KakaoLoginButton = () => {
   const { data: session } = useSession();
-  console.log( "세션 정보",session);
+  console.log("세션 정보", session);
 
   return (
     <div>
       {session ? (
         <div>
           <p>환영합니다, {session.user?.name}님!</p>
-          <button onClick={() => signOut()}>로그아웃</button>
+          <button className={styles.btn} onClick={() => signOut()}>
+            로그아웃
+          </button>
         </div>
       ) : (
-        <button
-        className={styles.btn}
-        onClick={() => signIn("kakao")}>카카오 로그인</button>
+        <button className={styles.btn} onClick={() => signIn("kakao")}>
+          카카오 로그인
+        </button>
       )}
     </div>
   );
