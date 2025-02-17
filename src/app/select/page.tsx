46aecee5991b2950248206e18c94805ebd10014d
jodasom <jodasom0216@gmail.com>
2025-02-17 15:08:08 +0900

    feat :  게임 버튼 css 이미지 구현

--- src/app/select/page.tsx
+++ src/app/select/page.tsx
@@ -1,7 +1,12 @@
+import styles from "./select.module.css";
+
 const Select = () => {
-    return ( <div>
-        
-    </div> );
-}
+  return (
+    <div className={styles.container}>
+      <button className={`${styles.btn} ${styles.startBtn}`}>게임 시작</button>
+      <button className={`${styles.btn} ${styles.reviewBtn}`}>단어 복습</button>
+    </div>
+  );
+};
 
 export default Select;
